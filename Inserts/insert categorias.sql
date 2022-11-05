7f474534cf714323e41e842310081e21bb39bf08
Alejandro Meneses Roldan <amenesesr82@gmail.com>
2022-11-05 15:26:42 -0500

    se corrige la indentación

--- Inserts/insert categorias.sql
+++ Inserts/insert categorias.sql
@@ -4,10 +4,10 @@ USE Import_tech;
 # Se insertan los datos de prueba en la tabla categorias.
 INSERT INTO categorias (nombre_categorias, descripcion_categorias)
 VALUES 	("Categoria de pruebas 1", "Descripcion de la categoria de pruebas 1"),
-		("Categoria de pruebas 2", "Descripcion de la categoria de pruebas 2"),
-		("Categoria de pruebas 3", "Descripcion de la categoria de pruebas 3"),
-		("Categoria de pruebas 4", "Descripcion de la categoria de pruebas 4"),
-		("Categoria de pruebas 5", "Descripcion de la categoria de pruebas 5");
+	("Categoria de pruebas 2", "Descripcion de la categoria de pruebas 2"),
+	("Categoria de pruebas 3", "Descripcion de la categoria de pruebas 3"),
+	("Categoria de pruebas 4", "Descripcion de la categoria de pruebas 4"),
+	("Categoria de pruebas 5", "Descripcion de la categoria de pruebas 5");
 
 # Se muestran todos los datos de la tabla categorias para verificar su contenido.
 SELECT * FROM categorias;
--- Inserts/insert detalleventas.sql
+++ Inserts/insert detalleventas.sql
@@ -4,21 +4,21 @@ USE Import_tech;
 # Se insertan los datos de prueba en la tabla detalleventas.
 INSERT INTO detalleventas (cantidad_detalleventas, valorventa_detalleventas, id_productos, consecutivo_ventas)
 VALUES 	(4, 126750, 2, 4),
-		(1, 326000, 6, 4),
-		(3, 420300, 13, 4),
-		(3, 350650, 12, 5),
-		(3, 49550, 19, 5),
-		(2, 475300, 3, 6),
-		(1, 625000, 20, 6),
-		(8, 235000, 5, 6),
-		(5, 546500, 9, 6),
-		(4, 129500, 14, 7),
-		(1, 346800, 11, 7),
-		(1, 200500, 2, 8),
-		(2, 197500, 16, 8),
-		(6, 64500, 8, 9),
-		(3, 356000, 20, 10),
-		(4, 289500, 17, 10);
+	(1, 326000, 6, 4),
+	(3, 420300, 13, 4),
+	(3, 350650, 12, 5),
+	(3, 49550, 19, 5),
+	(2, 475300, 3, 6),
+	(1, 625000, 20, 6),
+	(8, 235000, 5, 6),
+	(5, 546500, 9, 6),
+	(4, 129500, 14, 7),
+	(1, 346800, 11, 7),
+	(1, 200500, 2, 8),
+	(2, 197500, 16, 8),
+	(6, 64500, 8, 9),
+	(3, 356000, 20, 10),
+	(4, 289500, 17, 10);
 
 # Se muestran todos los datos de la tabla detalleventas para verificar su contenido.
 SELECT * FROM detalleventas;
