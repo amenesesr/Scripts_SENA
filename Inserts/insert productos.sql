e3ffe1bf0b9a926e656aa3e41e81d4d5da959a7b
Alejandro Meneses Roldan <amenesesr82@gmail.com>
2022-11-28 10:34:40 -0500

    Update insert productos.sql
    se están agregando productos mas reales a la tabla productos, por ahora los ID de la categoría se ponen esos porque habría que empezar todo desde cero para que queden de la mejor manera

--- Inserts/insert productos.sql
+++ Inserts/insert productos.sql
@@ -3,26 +3,114 @@ USE Import_tech;
 
 # Se insertan los datos de prueba en la tabla productos.
 INSERT INTO productos (nombre_productos, precio_productos, stock_productos, NIP_proveedores, id_categorias)
-VALUES	("Producto de Pruebas 1", 256500, 23, 951623847, 5),
-	("Producto de Pruebas 2", 35000,93,592635487, 3),
-	("Producto de Pruebas 3", 26500, 21, 123456789, 1),
-	("Producto de Pruebas 4", 145000, 58, 963852741, 2),
-	("Producto de Pruebas 5", 23000, 49, 789456123, 4),
-	("Producto de Pruebas 6", 45500, 38, 741963852, 5),
-	("Producto de Pruebas 7", 86000,31,951623847, 1),
-	("Producto de Pruebas 8", 15500,59,741963852, 2),
-	("Producto de Pruebas 9", 210000, 73, 741963852, 4),
-	("Producto de Pruebas 10", 123000, 48, 987654321, 3),
-	("Producto de Pruebas 11", 64500, 21, 741963852, 1),
-	("Producto de Pruebas 12", 315000,3,321654987, 2),
-	("Producto de Pruebas 13", 51500, 64, 963852741, 5),
-	("Producto de Pruebas 14", 90500, 98, 741963852 , 4),
-	("Producto de Pruebas 15", 29500, 47, 123456789, 2),
-	("Producto de Pruebas 16", 235000, 76 ,741963852, 1),
-	("Producto de Pruebas 17", 186000, 9, 789456123, 3),
-	("Producto de Pruebas 18", 76000, 12, 987654321, 5),
-	("Producto de Pruebas 19", 88500,89,592635487, 4),
-	("Producto de Pruebas 20", 146000,59,321654987, 3);
+SELECT * FROM (SELECT 	"Microsoft Windows 11" AS nombre_productos, 
+						"250000" AS precio_productos,
+                        "50" AS stock_productos,
+                        "54637281" AS NIP_proveedores,
+                        "13" AS id_categorias) AS tmp
+WHERE NOT EXISTS (
+    SELECT nombre_productos FROM productos WHERE nombre_productos = "Windows 11"
+) LIMIT 1;
+
+# Se insertan los datos de prueba en la tabla productos.
+INSERT INTO productos (nombre_productos, precio_productos, stock_productos, NIP_proveedores, id_categorias)
+SELECT * FROM (SELECT 	"Microsoft Office 2021" AS nombre_productos, 
+						"200000" AS precio_productos,
+                        "50" AS stock_productos,
+                        "333666999" AS NIP_proveedores,
+                        "13" AS id_categorias) AS tmp
+WHERE NOT EXISTS (
+    SELECT nombre_productos FROM productos WHERE nombre_productos = "Microsoft Office 2021"
+) LIMIT 1;
+
+INSERT INTO productos (nombre_productos, precio_productos, stock_productos, NIP_proveedores, id_categorias)
+SELECT * FROM (SELECT 	"Microsoft Office 2021" AS nombre_productos, 
+						"200000" AS precio_productos,
+                        "50" AS stock_productos,
+                        "333666999" AS NIP_proveedores,
+                        "13" AS id_categorias) AS tmp
+WHERE NOT EXISTS (
+    SELECT nombre_productos FROM productos WHERE nombre_productos = "Microsoft Office 2021"
+) LIMIT 1;
+
+INSERT INTO productos (nombre_productos, precio_productos, stock_productos, NIP_proveedores, id_categorias)
+SELECT * FROM (SELECT 	"Adobe PhotoShop" AS nombre_productos, 
+						"180000" AS precio_productos,
+                        "30" AS stock_productos,
+                        "8776665555" AS NIP_proveedores,
+                        "13" AS id_categorias) AS tmp
+WHERE NOT EXISTS (
+    SELECT nombre_productos FROM productos WHERE nombre_productos = "Adobe PhotoShop"
+) LIMIT 1;
+
+INSERT INTO productos (nombre_productos, precio_productos, stock_productos, NIP_proveedores, id_categorias)
+SELECT * FROM (SELECT 	"Eset NOD 32" AS nombre_productos, 
+						"120000" AS precio_productos,
+                        "25" AS stock_productos,
+                        "212223242" AS NIP_proveedores,
+                        "13" AS id_categorias) AS tmp
+WHERE NOT EXISTS (
+    SELECT nombre_productos FROM productos WHERE nombre_productos = "Eset NOD 32"
+) LIMIT 1;
+
+INSERT INTO productos (nombre_productos, precio_productos, stock_productos, NIP_proveedores, id_categorias)
+SELECT * FROM (SELECT 	"Autocad 2022" AS nombre_productos, 
+						"300000" AS precio_productos,
+                        "10" AS stock_productos,
+                        "627364590" AS NIP_proveedores,
+                        "13" AS id_categorias) AS tmp
+WHERE NOT EXISTS (
+    SELECT nombre_productos FROM productos WHERE nombre_productos = "Autocad 2022"
+) LIMIT 1;
+
+INSERT INTO productos (nombre_productos, precio_productos, stock_productos, NIP_proveedores, id_categorias)
+SELECT * FROM (SELECT 	"GTA V" AS nombre_productos, 
+						"115000" AS precio_productos,
+                        "20" AS stock_productos,
+                        "987654321" AS NIP_proveedores,
+                        "13" AS id_categorias) AS tmp
+WHERE NOT EXISTS (
+    SELECT nombre_productos FROM productos WHERE nombre_productos = "GTA V"
+) LIMIT 1;
+
+INSERT INTO productos (nombre_productos, precio_productos, stock_productos, NIP_proveedores, id_categorias)
+SELECT * FROM (SELECT 	"FIFA 22" AS nombre_productos, 
+						"98500" AS precio_productos,
+                        "35" AS stock_productos,
+                        "337744665" AS NIP_proveedores,
+                        "13" AS id_categorias) AS tmp
+WHERE NOT EXISTS (
+    SELECT nombre_productos FROM productos WHERE nombre_productos = "FIFA 22"
+) LIMIT 1;
+
+INSERT INTO productos (nombre_productos, precio_productos, stock_productos, NIP_proveedores, id_categorias)
+SELECT * FROM (SELECT 	"Teclado Samurai Maxel USB" AS nombre_productos, 
+						"120000" AS precio_productos,
+                        "10" AS stock_productos,
+                        "123456789" AS NIP_proveedores,
+                        "14" AS id_categorias) AS tmp
+WHERE NOT EXISTS (
+    SELECT nombre_productos FROM productos WHERE nombre_productos = "Teclado Samurai Maxel USB"
+) LIMIT 1;
+
+INSERT INTO productos (nombre_productos, precio_productos, stock_productos, NIP_proveedores, id_categorias)
+SELECT * FROM (SELECT 	"Teclado Genius Bluetooth" AS nombre_productos, 
+						"85000" AS precio_productos,
+                        "16" AS stock_productos,
+                        "951623847" AS NIP_proveedores,
+                        "14" AS id_categorias) AS tmp
+WHERE NOT EXISTS (
+    SELECT nombre_productos FROM productos WHERE nombre_productos = "Teclado Genius Bluetooth"
+) LIMIT 1;
+
+SELECT * FROM (SELECT 	"Teclado LogiTech F50 BlueTooth" AS nombre_productos, 
+						"145000" AS precio_productos,
+                        "5" AS stock_productos,
+                        "554321077" AS NIP_proveedores,
+                        "14" AS id_categorias) AS tmp
+WHERE NOT EXISTS (
+    SELECT nombre_productos FROM productos WHERE nombre_productos = "Teclado LogiTech F50 BlueTooth"
+) LIMIT 1;
 
 #Se muestran todos los datos de la tabla productos para verificar su contenido.
 SELECT * FROM productos;
